b9a0ef0ea9ad3327d8714a5ac19bd6dad18f625b
Artsiom_Kazlouski <tema.15.10mail@gmail.com>
2019-05-16 12:52:44 +0300

    finally remove unused module

--- app/src/main/java/com/ank/rxr/bluetooth/BluetoothManager.kt
+++ app/src/main/java/com/ank/rxr/bluetooth/BluetoothManager.kt
@@ -3,21 +3,20 @@ package com.ank.rxr.bluetooth
 import com.ank.core.BluetoothDevice
 import com.ank.core.IBluetoothDeviceManager
 
-class BluetoothManager: IBluetoothDeviceManager{
+class BluetoothDeviceManager: IBluetoothDeviceManager{
     override fun saveCurrentDevise(device: BluetoothDevice) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun startDiscovery(onNext: BluetoothDevice) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun stopDiscovery() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun getCurrentDevice(): BluetoothDevice {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return BluetoothDevice("s")
     }
-
 }
